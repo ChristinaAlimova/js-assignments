c561708e2b81b2690f40165fe84481c08a5f13ec
ChristinaAlimova <41754421+ChristinaAlimova@users.noreply.github.com>
2019-04-14 20:16:01 +0300

    feat: add yield tasks

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -33,7 +33,17 @@
  *
  */
 function* get99BottlesOfBeer() {
-    throw new Error('Not implemented');
+    let result = [];
+	for(let i = 99; i > 1; i--) {
+		let ind = i-1;
+		result.push(i + ' bottles of beer on the wall, ' + i + ' bottles of beer.');
+		result.push('Take one down and pass it around, ' + ind + ' bottles of beer on the wall.');
+	}
+	result.push('1 bottle of beer on the wall, 1 bottle of beer.');
+	result.push('Take one down and pass it around, no more bottles of beer on the wall.');
+	result.push('No more bottles of beer on the wall, no more bottles of beer.');
+    result.push('Go to the store and buy some more, 99 bottles of beer on the wall.');
+    return result;
 }
 
 
@@ -47,7 +57,7 @@ function* get99BottlesOfBeer() {
  *
  */
 function* getFibonacciSequence() {
-    throw new Error('Not implemented');
+    
 }
 
 
